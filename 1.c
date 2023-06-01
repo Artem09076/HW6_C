dadf4589573fe5e7c1c81a46cbdc038996f819db
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-06-01 18:32:56 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b,c,d;
     scanf("%d %d %d %d",&a,&b,&c,&d);
-    int x=a+((c - a % d) % d);
+    int x=a+((c - a % d) % d);//Эта штука проверяет больше ли оно a(или равно) и даёт остаток c при делении на d
     while (x<=b)
     {
         printf("%d ",x);
