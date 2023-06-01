708fdc8bcee46b46aee7337de4839ada56a63689
Artem09076 <120830078+Artem09076@users.noreply.github.com>
2023-06-01 18:35:08 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int sumDigits(int *num){
     int summ=0;
-    while (*num!=0)
+    while (*num!=0)//Пока num не равн 0 прибовляем к summ остаток от деления и с помощью указателей записываем в num целочисленное деление
     {
         summ+=(*num%10);
         *num=*num/10;
